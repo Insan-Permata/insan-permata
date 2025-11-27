1e545f7c2a355d600fcd3d73f24f63790dccc2ff
golden014 <josuagolden14@gmail.com>
2025-11-27 19:24:33 +0700

    update next config

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    remotePatterns: [],
+    remotePatterns: [new URL(process.env.SUPABASE_STORAGE_BUCKET_URL_PREFIX! + "/**")],
     formats: ['image/avif', 'image/webp'],
     qualities: [25, 50, 75],
   },
